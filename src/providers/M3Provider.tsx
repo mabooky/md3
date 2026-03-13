bf55a0278eff028341aac8a6f7dbbff9b7d730af
Heungrok <mabookyggl@gmail.com>
2026-03-14 00:54:03 +0900

    refactor: M3Provider 구현을 단일 파일 구현으로 수정

--- src/providers/M3Provider.tsx
+++ src/providers/M3Provider.tsx
@@ -1,18 +1,48 @@
 'use client';
 
-import { M3MotionSchemeProvider } from "./M3MotionSchemeProvider";
-import { M3ThemeProvider } from "./M3ThemeProvider";
+import { createContext, useContext } from "react";
+import { ThemeProvider } from "next-themes";
+
+type M3ContextType = {
+    iconStyle: 'outlined' | 'rounded' | 'sharp';
+    motionScheme: 'standard' | 'expressive';
+};
+
+// createContext의 기본값을 null로 설정해도 useM3Context의 반환 타입이 non-null로 추론됨
+const M3Context = createContext<M3ContextType | null>(null);
+
+export function useM3Context() {
+    const context = useContext(M3Context);
+
+    if (!context) {
+        throw new Error("useM3Context 함수는 M3Provider 컴포넌트 내부에서만 사용되어야 합니다.");
+    }
+
+    return context;
+}
 
 export function M3Provider({
+    iconStyle = 'outlined',
+    motionScheme = 'expressive',
     children
 }: Readonly<{
+    iconStyle?: 'outlined' | 'rounded' | 'sharp';
+    motionScheme?: 'standard' | 'expressive';
     children?: React.ReactNode;
 }>) {
     return (
-        <M3ThemeProvider>
-            <M3MotionSchemeProvider>
+        <M3Context value={{ iconStyle, motionScheme }}>
+
+            <ThemeProvider
+                themes={['system', 'light', 'light-medium-contrast', 'light-high-contrast', 'dark', 'dark-medium-contrast', 'dark-high-contrast']}
+                enableSystem={true}
+                defaultTheme="system"
+                disableTransitionOnChange>
+
                 {children}
-            </M3MotionSchemeProvider>
-        </M3ThemeProvider>
+
+            </ThemeProvider>
+
+        </M3Context>
     );
 }
