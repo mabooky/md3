dcde967f01b58812f835cfa52c1fe9c48271bb21
Heungrok <mabookyggl@gmail.com>
2026-04-02 08:54:39 +0900

    feat: 라이브러리 내보내기에 StateLayer 관련 항목 추가

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import { AppBarHeadline, AppBarLeadingButton, AppBarRoot, AppBarTrailingButton } from "./components/AppBar"
 import { NavigationBarRoot, NavigationBarItem } from "./components/NavigationBar"
 
-export * from "./components/IconButton"
+export * from "./core/Container"
+export * from "./core/Icon"
+export * from "./core/InteractiveComponent"
+export * from "./core/StatefulContainer"
+export * from "./core/StateLayer"
+export * from "./core/TouchTarget"
+
 export const AppBar = Object.assign(AppBarRoot, {
     LeadingButton: AppBarLeadingButton,
     TrailingButton: AppBarTrailingButton,
@@ -11,17 +17,12 @@ export * from "./components/Button"
 export * from "./components/ButtonGroup"
 export * from "./components/Card"
 export * from "./components/FAB"
+export * from "./components/IconButton"
 export const NavigationBar = Object.assign(NavigationBarRoot, {
     Item: NavigationBarItem,
 })
 export * from "./components/Pane"
 export * from "./components/Tooltip"
-
-export * from "./core/Container"
-export * from "./core/Icon"
-export * from "./core/InteractiveComponent"
-export * from "./core/StatefulContainer"
-export * from "./core/TouchTarget"
 
 export * from "./providers/M3Provider"
 
