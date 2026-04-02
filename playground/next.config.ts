5b63ba0849fab86684fa87567758b1541667213f
Heungrok <mabookyggl@gmail.com>
2026-04-02 09:06:34 +0900

    build: 변경된 라이브러리 이름에 맞게 next.config.ts 수정

--- playground/next.config.ts
+++ playground/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  transpilePackages: ["@mabooky/tailored-m3"]
+  transpilePackages: ["@mabooky/md3"],
 };
 
 export default nextConfig;
